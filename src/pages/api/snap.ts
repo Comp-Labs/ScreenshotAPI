148c7d5f54cf5523f2d64c7f3feffde810765fd1
Rudra Sen <rudra_sen@outlook.com>
2023-06-06 12:17:15 +0530

    Update code

--- src/pages/api/snap.ts
+++ src/pages/api/snap.ts
@@ -6,30 +6,32 @@ export const config = {
 }
 
 export default async function snap(request: VercelRequest, response: VercelResponse) {
-    try {
-        let url = decodeURIComponent(`${request.query.url}`);
+  try {
+    let url = decodeURIComponent(request.query.url as string);
 
-        // Prepend "http://" if the URL doesn't start with a protocol
-        if (!/^https?:\/\//i.test(url)) {
-            url = `http://${url}`;
-        }
-        const browser = await createBrowser();
-        const browserless = await browser.createContext();
-
-        // Set a custom viewport size
-        // await page.setViewport({ width: 1920, height: 1080 });
-
-        // await page.goto(url);
-        const screenshot = await browserless.screenshot(url);
-
-        await browserless.destroyContext();
-        await browser.close();
-
-        response.setHeader('Content-Type', 'image/jpg');
-        response.setHeader('Cache-Control', 'max-age=0, s-maxage=86400');
-        response.send(screenshot);
-    } catch (error) {
-        console.error(error);
-        response.status(500).json({ error: 'An error occurred while capturing the screenshot' });
+    // Prepend "http://" if the URL doesn't start with a protocol
+    if (!/^https?:\/\//i.test(url)) {
+      url = `http://${url}`;
     }
+
+    const browser = await createBrowser();
+    const browserless = await browser.createContext();
+    // const page: Page = await browserless.newPage();
+
+    // Set a custom viewport size
+    // await page.setViewport({ width: 1920, height: 1080 });
+
+    // await page.goto(url);
+    const screenshot = await browserless.screenshot(url);
+
+    await browserless.destroyContext();
+    await browser.close();
+
+    response.setHeader('Content-Type', 'image/jpeg');
+    response.setHeader('Cache-Control', 'max-age=0, s-maxage=86400');
+    response.send(screenshot);
+  } catch (error) {
+    console.error(error);
+    response.status(500).json({ error: 'An error occurred while capturing the screenshot' });
+  }
 }
