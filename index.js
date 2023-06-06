a1e3b98ebdef35f2629de0daa00f764c7e082863
Rudra Sen <rudra_sen@outlook.com>
2023-06-06 12:38:00 +0530

    Remove Next.js

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
+const express = require('express');
 const createBrowser = require('browserless')({ lossyDeviceName: true });
 
-export const config = {
-    runtime: 'edge',
-}
+const app = express();
+const port = process.env.PORT || 3000;
 
-export default async function snap(request: VercelRequest, response: VercelResponse) {
+app.get('/snap', async (req, res) => {
   try {
-    let url = decodeURIComponent(request.query.url as string);
+    let url = decodeURIComponent(req.query.url);
 
     // Prepend "http://" if the URL doesn't start with a protocol
     if (!/^https?:\/\//i.test(url)) {
@@ -16,7 +15,7 @@ export default async function snap(request: VercelRequest, response: VercelRespo
 
     const browser = await createBrowser();
     const browserless = await browser.createContext();
-    // const page: Page = await browserless.newPage();
+    // const page = await browserless.newPage();
 
     // Set a custom viewport size
     // await page.setViewport({ width: 1920, height: 1080 });
@@ -27,11 +26,14 @@ export default async function snap(request: VercelRequest, response: VercelRespo
     await browserless.destroyContext();
     await browser.close();
 
-    response.setHeader('Content-Type', 'image/jpeg');
-    response.setHeader('Cache-Control', 'max-age=0, s-maxage=86400');
-    response.send(screenshot);
+    res.set('Content-Type', 'image/jpeg');
+    res.send(screenshot);
   } catch (error) {
     console.error(error);
-    response.status(500).json({ error: 'An error occurred while capturing the screenshot' });
+    res.status(500).json({ error: 'An error occurred while capturing the screenshot' });
   }
-}
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
