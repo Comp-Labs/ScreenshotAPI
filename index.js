b1687c1d51405c4c23514b3387e3b0493d9f8766
Rudra Sen <rudra_sen@outlook.com>
2023-06-27 18:21:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import express from 'express',
-import createBrowser from 'browserless',
+import {express} from 'express';
+import {createBrowser} from 'browserless';
 
 const app = express();
 const port = process.env.PORT || 3000;
