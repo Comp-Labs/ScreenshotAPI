304b5db7bbd3e9fcc09983559a65330543c503cf
Rudra Sen <rudra_sen@outlook.com>
2024-06-22 19:50:14 +0530

    Update Port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { executablePath } = require('puppeteer')
 require('dotenv').config()
 
 const app = express()
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 10000
 const cache = {}
 
 app.get('/', async (request, response) => {
